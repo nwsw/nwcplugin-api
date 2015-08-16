e6dac27c84a52fe7ae2bab56676ea49a2ebe720e
Noteworthy Software, Inc. <online@noteworthysoftware.com>
2015-08-16 09:01:00 -0400

    Update Simple.demo.nwcuser.lua

--- examples/Simple.demo.nwcuser.lua
+++ examples/Simple.demo.nwcuser.lua
@@ -1,52 +1,67 @@
 -- Version 0.0
 
---[[-----------------------------------------------------------------------------------------
-Simple.demo
+--[[--------------------------------------------------------------------------
+This is a new plugin object. This comnment will appear each time a user
+selects this object type for insertion into the staff.
 
-This is the object code for User objects of type Simple.demo.
+@field1
+This is the help text for field1.
 
---]]-----------------------------------------------------------------------------------------
+@field2
+This is the help text for field2.
+
+@field3
+This is the help text for field3.
+
+--]]--------------------------------------------------------------------------
 
 -- our object type is passed into the script
 local userObjTypeName = ...
 
----------------------------------------------------------------------------------------------
+-- things work best when all fields supported by the plugin are published in a
+-- spec table
+local object_spec = {
+	{ id='field1', label='Field 1', type='text', default='' },
+	{ id='field2', label='Field 2', type='enum', default='value1', list={'value1','value2','value3'} },
+	{ id='field3', label='Field 3', type='int', default=0, min=0, max=20, step=2 },
+}
+
+------------------------------------------------------------------------------
 -- the 'create' method is used to establish the object properties that will
 -- control our plugin object
 local function do_create(t)
-	t.Data = 1
+	t.field1 = 'Hello'
 end
 
----------------------------------------------------------------------------------------------
--- the 'audit' method is called whenever something has changed in the area of the object,
--- including the object itself
+------------------------------------------------------------------------------
+-- the 'audit' method is called whenever the object is first opened or pasted
+-- into the editor
 local function do_audit(t)
-	if not (tonumber(t.Data) == 1) then
-		-- for purposes of this demo, we ensure that t.Data is always 1
-		t.Data = 1
-	end
 end
 
----------------------------------------------------------------------------------------------
--- the 'spin' method is called whenever the user applied the +/- keys to our 
+------------------------------------------------------------------------------
+-- the 'spin' method is called whenever the user applied the +/- keys to our
 -- object from within the editor
 local function do_spin(t,d)
-	t.Data = (tonumber(t.Data) or 0) + d
+	t.field3 = t.field3 + d
+
+	-- assigning this to itself enforces the spec table min and max
+	t.field3 = t.field3
 end
 
----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 -- the 'transpose' method is called whenever the user runs the Transpose Staff
 -- command from within the editor
 local function do_transpose(t,semitones,notepos,updpatch)
 end
 
----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 -- the 'play' method is called whenever our notatation is compiled into a
 -- midi performance
 local function do_play(t)
 end
 
----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 -- the 'width' and 'draw' methods are combined here, and are used in the
 -- formatting and display of our object on the NWC staff
 --
@@ -62,17 +77,19 @@ local function do_draw(t)
 		return w
 	end
 
-	nwcdraw.alignText('baseline','right')
+	nwcdraw.alignText('baseline','left')
+	nwcdraw.moveTo(-nwcdraw.width())
 	nwcdraw.text(userObjTypeName)
 end
 
----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 -- all object plug-ins must return the methods that they support in a
 -- standard method table; uncomment any additional methods as needed
 
 return {
+	spec		= object_spec,
 	create		= do_create,
-	audit		= do_audit,
+--	audit		= do_audit,
 	spin		= do_spin,
 --	transpose	= do_transpose,
 --	play		= do_play,
